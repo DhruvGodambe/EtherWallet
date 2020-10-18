fb89abcb1e17bd4242c9da193f3b2b33efbdce13
DhruvGodambe <dhruvrockstone@gmail.com>
2020-10-18 12:34:49 +0530

    Updates

---
+++ precache-manifest.9a81edd2e0dff20463232197ca408373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218876f2113d5e7ceab6f44a2962b570",
+    "url": "/EtherWallet/index.html"
+  },
+  {
+    "revision": "9e8c5d0cee79adc3b7db",
+    "url": "/EtherWallet/static/css/main.3996b5e3.chunk.css"
+  },
+  {
+    "revision": "872c0364203944681d0f",
+    "url": "/EtherWallet/static/js/2.1e2b8cb9.chunk.js"
+  },
+  {
+    "revision": "9e8c5d0cee79adc3b7db",
+    "url": "/EtherWallet/static/js/main.7cadb704.chunk.js"
+  },
+  {
+    "revision": "950ca15444b1c334702e",
+    "url": "/EtherWallet/static/js/runtime-main.45b62b64.js"
+  }
+]);
